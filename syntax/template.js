3d4f985b42e00da8b215e14cd396256eb151a14a
Kimm <hyunsung.kim@zeals.co.jp>
2020-06-14 18:51:14 +0900

    try url method

--- syntax/template.js
+++ syntax/template.js
@@ -1,8 +1,9 @@
 const name = "Kimm"
 var paragraph = 'Lorem Ipsum ' + name + '\n\nis simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.' + name
-console.log(paragraph)
+// console.log(paragraph)
 var a = '1';
 
 var paragraph = `Lorem Ipsum ${name}
 
-is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.` + name
+is simply dummy text ${3 * 5}of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ${name}`;
+console.log(paragraph)
